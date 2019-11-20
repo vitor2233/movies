e08ca6aade0ca60f54f7911ed28664cb68c73172
vitor2233 <vitorguilherme2233@gmail.com>
2019-11-20 13:18:24 -0300

    Updates

---
+++ precache-manifest.0a4905af04f1a4acffe193910381ce0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7ca172146a98ff04327013733a2d3b",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "4a2c304aaa8677f76836",
+    "url": "/movies/static/css/main.042c6eb5.chunk.css"
+  },
+  {
+    "revision": "edf591a79825417623b8",
+    "url": "/movies/static/js/2.1f44e971.chunk.js"
+  },
+  {
+    "revision": "4a2c304aaa8677f76836",
+    "url": "/movies/static/js/main.d99a9317.chunk.js"
+  },
+  {
+    "revision": "44dd2604e638b555fce0",
+    "url": "/movies/static/js/runtime-main.4fb37c4b.js"
+  }
+]);
